d611f3d0952db41a926f7283f5a591ac3cfaa3aa
Mezen jebali <jebali.mazen@gmail.com>
2024-09-03 21:34:54 +0000

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    sample.env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,10 +18,57 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const vlidateDate = (req, res, next) => {
+  const parameter = req.params.date,
+    nowUnix = Date.now();
+
+  if (!parameter) {
+    req.paramType = "NOW";
+  }
+  else if (!isNaN(parameter) && parameter > 0 && parameter <= nowUnix) {
+    req.paramType = "UNIX";
+  }
+  else if (new Date(parameter) != "Invalid Date") {
+    req.paramType = "UTC";
+  }
+  else {
+    req.paramType = "ERROR";
+  };
+  next();
+}
+
+const GenerateResult = (req, res, next) => {
+  if (req.paramType == "UNIX") {
+    const UTC = new Date(Number(req.params.date)).toGMTString();
+    req.resultDate = { unix: req.params.date, utc: UTC };
+  }
+  else if (req.paramType == "UTC") {
+    const UNIX = new Date(req.params.date).getTime();
+    req.resultDate = {
+      unix: UNIX,
+      utc: new Date(req.params.date).toGMTString()
+    }
+  }
+  else if (req.paramType == "NOW") {
+    req.resultDate = {
+      unix: new Date().getTime(),
+      utc: new Date().toGMTString()
+    }
+  }
+  else {
+    req.resultDate = { error: "Invalid Date" }
+
+  }
+  next();
+}
+
+app.get("/api/:date?", vlidateDate, GenerateResult, (req, res) => {
+  res.json(req.resultDate);
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
